698a54c6b4f6093133e48ffe1bfbce862bc84e47
Shaun Berkley <shaun@berkleysoftware.com>
2023-03-21 15:35:11 -0700

    Remove rollup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+const path = require('path');
+const { defineConfig } = require('vite');
+const vue = require('@vitejs/plugin-vue');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: 'Blueprint',
+      fileName: (format) => `blueprint.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   plugins: [vue()],
-})
+});
