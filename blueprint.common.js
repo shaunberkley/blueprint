fd88928c5d59f3005cde4c9bb8e96fedaa8f3d6c
Shaun Berkley <shaun@berkleysoftware.com>
2023-03-21 15:24:33 -0700

    Fix

--- blueprint.common.js
+++ blueprint.common.js
@@ -1,8 +1,8 @@
-import vue from 'rollup-plugin-vue';
-import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
+import vuePlugin from 'rollup-plugin-vue';
+import { babel } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/lib/index.ts',
@@ -20,13 +20,13 @@ export default {
       file: 'dist/blueprint.min.js',
       format: 'iife',
       name: 'Blueprint',
-      plugins: [terser()]
     }
   ],
   plugins: [
-    resolve(),
+    nodeResolve(),
     commonjs(),
-    vue({
+    terser(),
+    vuePlugin({
       css: true,
       compileTemplate: true,
       template: {
