366ef8799784cca700706df2142aca9b7b7ba983
Shaun Berkley <shaun@berkleysoftware.com>
2023-08-14 10:08:02 -0700

    Load CSS with js file

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), dts()],
+    plugins: [vue(), dts(), cssInjectedByJsPlugin()],
     build: {
         lib: {
             entry: resolve(__dirname, "src/index.ts"),
